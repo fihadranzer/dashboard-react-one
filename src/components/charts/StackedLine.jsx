f601f4f7915c749ce74863f473ffdbe8027940c6
fiha91 <rhfiha@gmail.com>
2022-12-29 17:58:18 +0600

    up to employees is done

--- src/components/charts/StackedLine.jsx
+++ src/components/charts/StackedLine.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const StackedLine = () => {
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
+
+const StackedLine = ({ width, height }) => {
   return (
-    <div>StackedLine</div>
-  )
-}
+    <ChartComponent
+      width={width}
+      height={height}
+      id="Charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{enable:true}}
+      legendSettings={{background:"white"}}
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index)=><SeriesDirective key={index} {...item}/>)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
+};
 
-export default StackedLine
+export default StackedLine;
--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Inject,
+  Toolbar
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Employee = () => {
   return (
-    <div>Employee</div>
-  )
-}
+    <div className="m-2 md:m-10 p-2 md:p-2 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        
+        dataSource={employeesData}
+        allowPaging
+        toolbar={['Search']}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
+};
 
-export default Employee
+export default Employee;
